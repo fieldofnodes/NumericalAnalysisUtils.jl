be8d3902dffabad732f2f22b96947dc5c6a8e416
Documenter.jl <documenter@juliadocs.github.io>
2024-08-09 20:43:19 +0000

    build based on 08470e9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NumericalAnalysisUtils","category":"page"},{"location":"#NumericalAnalysisUtils","page":"Home","title":"NumericalAnalysisUtils","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NumericalAnalysisUtils.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NumericalAnalysisUtils]","category":"page"}]
+}
